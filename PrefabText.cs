4ae76e9f8ea95099d4740798453710a329306b4a
Opportunity1234 <1347009082@qq.com>
2018-02-27 09:00:36 +0800

    加中
    】】

--- PrefabText.cs
+++ PrefabText.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 ------------------------------------++
+
+]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 public class PrefabText : MonoBehaviour {
 //	预设体的对象
 	public GameObject myPrefab;
